46ed63e7369788363a21891d942b05c692dff858
IlyaNSV <56345031+IlyaNSV@users.noreply.github.com>
2020-05-23 18:25:27 +0600

    Домашняя работа за третий урок

---
+++ lesson3_7.js
@@ -0,0 +1,7 @@
+let row='x';
+let new_row='x';
+console.log(row)
+for (let i = 0; i<=18; i++){
+    new_row = new_row.concat(row)
+    console.log(new_row)
+} // уверен, что это самый неоптимальный способ
